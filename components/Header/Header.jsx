34b99d7788a40fcaece918d52613171f7808d5cf
Ragib Al Asad <ragibalasad@gmail.com>
2024-07-03 00:35:13 +0600

    fixed stupid &apos; excape error

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="absolute inset-0 flex h-screen">
         <div className="w-1/2 left-half flex justify-between">
           <div className="intro container p-10 mt-28 mr-32 text-right">
-            <p className="text-2xl text-ff">Hi 👋 I'm</p>
+            <p className="text-2xl text-ff">Hi 👋 I&apos;m</p>
             <p className="text-7xl text-ff font-bold">
               RAGIB <br /> AL ASAD
             </p>
