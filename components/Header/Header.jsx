c144b118a0596cfebbe8f6898ffcb321510f88a4
Ragib Al Asad <ragibalasad@gmail.com>
2024-07-03 00:28:51 +0600

    added intro in header section

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,12 +2,21 @@
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="absolute -z-50 flex w-full h-screen">
-        <div className="w-1/2 left-half bg-highlight"></div>
+    <header className="header relative">
+      <div className="absolute inset-0 flex h-screen">
+        <div className="w-1/2 left-half flex justify-between">
+          <div className="intro container p-10 mt-28 mr-32 text-right">
+            <p className="text-2xl text-ff">Hi 👋 I'm</p>
+            <p className="text-7xl text-ff font-bold">
+              RAGIB <br /> AL ASAD
+            </p>
+          </div>
+        </div>
         <div className="w-1/2 right-half"></div>
       </div>
-      <div className="container p-4 pt-16"></div>
+      <div className="container p-4 pt-16">
+        {/* Additional content or components can go here */}
+      </div>
     </header>
   );
 };
