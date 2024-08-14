9755d1cada9381ebd07b75981f54a68433c5fe78
Ragib Al Asad <ragibalasad@gmail.com>
2024-08-14 16:55:55 +0600

    Added parallax motion to color-bubbles

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,15 +1,46 @@
 "use client";
 
-import CodeWindow from "./CodeWindow";
+import { React, useRef } from "react";
 import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { useScroll, useTransform, motion } from "framer-motion";
+
+import CodeWindow from "./CodeWindow";
 
 const Hero = () => {
+  let ref = useRef();
+  let { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  let yPinkTransform = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  let yIndigoTransform = useTransform(scrollYProgress, [0, 1], ["0%", "250%"]);
+  let yGreenTransform = useTransform(scrollYProgress, [0, 1], ["0%", "600%"]);
+  let yYellowTransform = useTransform(scrollYProgress, [0, 1], ["0%", "-250%"]);
+
+  let yPink = yPinkTransform;
+  let yIndigo = yIndigoTransform;
+  let yGreen = yGreenTransform;
+  let yYellow = yYellowTransform;
+
   return (
     <section className="hero container relative mx-auto md:w-4/5">
-      <div className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 opacity-70 shadow-2xl shadow-pink-500 blur"></div>
-      <div className="blue-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500 opacity-70 shadow-2xl shadow-indigo-500 blur"></div>
-      <div className="green-bubble absolute bottom-1/2 right-1/3 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 opacity-70 shadow-2xl shadow-green-500 blur"></div>
-      <div className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500 opacity-70 shadow-2xl shadow-yellow-500 blur"></div>
+      <motion.div
+        style={{ y: yPink }}
+        className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 opacity-70 shadow-2xl shadow-pink-500 blur"
+      ></motion.div>
+      <motion.div
+        style={{ y: yIndigo }}
+        className="indigo-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500 opacity-70 shadow-2xl shadow-indigo-500 blur"
+      ></motion.div>
+      <motion.div
+        style={{ y: yGreen }}
+        className="green-bubble absolute bottom-1/2 right-1/3 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 opacity-70 shadow-2xl shadow-green-500 blur"
+      ></motion.div>
+      <motion.div
+        style={{ y: yYellow }}
+        className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500 opacity-70 shadow-2xl shadow-yellow-500 blur"
+      ></motion.div>
 
       <div className="relative grid w-full grid-cols-2">
         <div className="mt-32 justify-between">
