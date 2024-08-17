c0bb26c90f4a544ebae2e4cbfbf75ffb45b050d4
Ragib Al Asad <ragibalasad@gmail.com>
2024-08-18 04:07:16 +0600

    Generate project card dynamically

--- components/Work/Work.jsx
+++ components/Work/Work.jsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
+import ss_develeven from "../../public/images/screenshot-develeven.png";
+import ss_ragib_portfolio from "../../public/images/screenshot-ragib-portfolio.png";
+import { FaEye, FaGithub } from "react-icons/fa";
+
 const Work = () => {
+  const projects = [
+    {
+      title: "DevEleven Website",
+      image: ss_develeven,
+      live: "https://develeven.vercel.app/",
+      github: "https://github.com/DEVELEVEN-io/develeven-io",
+    },
+    {
+      title: "Ragib's Portfolio",
+      image: ss_ragib_portfolio,
+      live: "https://ragibalasad.me/",
+      github: "https://github.com/ragibalasad/ragib-portfolio",
+    },
+  ];
+
   return (
     <section className="work relative mt-24 bg-slate-800 max-sm:px-6 2xl:mt-32">
       <div className="placeHolderComponent container mx-auto h-auto py-20 sm:w-4/5">
         <div className="mb-8 flex max-sm:flex-col max-sm:items-center max-sm:gap-10">
           <h1 className="text-4xl font-semibold">Projects</h1>
           <ul className="flex gap-3 text-sm font-medium sm:ml-auto">
-            <li className="rounded-lg bg-slate-900/70 px-4 py-2 ring-1 ring-white/10">
+            <li className="rounded-lg bg-slate-900/70 px-4 py-2 ring-1 ring-inset ring-white/10">
               All
             </li>
             <li className="rounded-lg bg-slate-700/70 px-4 py-2">Frontend</li>
@@ -13,17 +33,63 @@ const Work = () => {
             <li className="rounded-lg bg-slate-700/70 px-4 py-2">Others</li>
           </ul>
         </div>
-        <div className="grid gap-6 lg:grid-cols-2 lg:grid-rows-2">
-          <div className="rounded-2xl bg-slate-900/70 p-12 ring-1 ring-inset ring-white/10">
-            <h1>Project 1</h1>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
-            ipsum, excepturi blanditiis laudantium nesciunt ipsam? Suscipit
-            assumenda totam, ipsum adipisci dolores repellat nihil veniam porro
-            mollitia quisquam asperiores obcaecati culpa. At, a incidunt
-            voluptate qui quidem aperiam amet adipisci sequi dignissimos id
-            cupiditate sit dolorum! Nulla, totam provident exercitationem optio
-            voluptatem beatae omnis, ex est a atque maxime delectus impedit!
+
+        <div className="grid gap-6 lg:grid-cols-3 lg:grid-rows-2">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="overflow-hidden rounded-2xl bg-slate-900/70 ring-1 ring-white/10"
+            >
+              <div className="overflow-hidden">
+                <Image
+                  className="transition-all hover:scale-110"
+                  src={project.image}
+                />
+              </div>
+              <div className="flex p-4 text-slate-300">
+                <h1 className="font-medium">{project.title}</h1>
+                <div className="ml-auto flex gap-4 text-xl">
+                  <a
+                    className="transition hover:text-cyan-400"
+                    href={project.live}
+                    target="_blank"
+                    rel=""
+                  >
+                    <FaEye />
+                  </a>
+                  <a
+                    className="transition hover:text-cyan-400"
+                    href={project.github}
+                    target="_blank"
+                    rel=""
+                  >
+                    <FaGithub />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="overflow-hidden rounded-2xl bg-slate-900/70 ring-1 ring-white/10">
+            <div className="overflow-hidden">
+              <Image
+                className="transition-all hover:scale-110"
+                src={ss_ragib_portfolio}
+              />
+            </div>
+            <div className="flex p-4 text-slate-300">
+              <h1 className="font-medium">Ragib's Portfolio</h1>
+              <div className="ml-auto flex gap-4 text-xl">
+                <a href="http://" target="_blank" rel="">
+                  <FaEye />
+                </a>
+                <a href="http://" target="_blank" rel="">
+                  <FaGithub />
+                </a>
+              </div>
+            </div>
           </div>
+
           <div className="rounded-2xl p-12 dark:bg-slate-900/70 dark:ring-1 dark:ring-inset dark:ring-white/10">
             <h1>Project 1</h1>
           </div>
