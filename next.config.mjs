ea651574c7055ad453d78f9aed9df01549720d12
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-23 14:27:42 +0600

    fix: removed image url config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
