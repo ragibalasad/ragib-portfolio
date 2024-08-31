ae0e79c36dcaf633cb729be2773a8e72bd731578
Ragib Al Asad <ragibalasad@gmail.com>
2024-08-31 16:07:01 +0600

    Separate Bubbles from Hero as a component

--- components/Hero/Bubbles.jsx
+++ components/Hero/Bubbles.jsx
@@ -1,7 +1,45 @@
 "use client";
 
+import { React, useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
+
 const Bubbles = () => {
-  return;
+  let ref = useRef();
+  let { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  let yPinkTransform = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  let yIndigoTransform = useTransform(scrollYProgress, [0, 1], ["0%", "250%"]);
+  let yGreenTransform = useTransform(scrollYProgress, [0, 1], ["0%", "600%"]);
+  let yYellowTransform = useTransform(scrollYProgress, [0, 1], ["0%", "-250%"]);
+
+  let yPink = yPinkTransform;
+  let yIndigo = yIndigoTransform;
+  let yGreen = yGreenTransform;
+  let yYellow = yYellowTransform;
+
+  return (
+    <>
+      <motion.div
+        style={{ y: yPink }}
+        className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 opacity-70 shadow-2xl shadow-pink-500 blur max-md:left-6"
+      ></motion.div>
+      <motion.div
+        style={{ y: yIndigo }}
+        className="indigo-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500 opacity-70 shadow-2xl shadow-indigo-500 blur"
+      ></motion.div>
+      <motion.div
+        style={{ y: yGreen }}
+        className="green-bubble absolute right-1/3 top-28 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 opacity-70 shadow-2xl shadow-green-500 blur max-md:right-6 max-md:top-52 2xl:top-1/4"
+      ></motion.div>
+      <motion.div
+        style={{ y: yYellow }}
+        className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500 opacity-70 shadow-2xl shadow-yellow-500 blur"
+      ></motion.div>
+    </>
+  );
 };
 
 export default Bubbles;
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,48 +1,13 @@
-"use client";
-
-import { React, useRef } from "react";
 import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
-import { useScroll, useTransform, motion } from "framer-motion";
 
 import CodeWindow from "./CodeWindow";
+import Bubbles from "./Bubbles";
 import { TextEffect } from "../TextEffect";
 
 const Hero = () => {
-  let ref = useRef();
-  let { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  let yPinkTransform = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
-  let yIndigoTransform = useTransform(scrollYProgress, [0, 1], ["0%", "250%"]);
-  let yGreenTransform = useTransform(scrollYProgress, [0, 1], ["0%", "600%"]);
-  let yYellowTransform = useTransform(scrollYProgress, [0, 1], ["0%", "-250%"]);
-
-  let yPink = yPinkTransform;
-  let yIndigo = yIndigoTransform;
-  let yGreen = yGreenTransform;
-  let yYellow = yYellowTransform;
-
   return (
     <section className="hero container relative mx-auto sm:w-4/5">
-      <motion.div
-        style={{ y: yPink }}
-        className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 opacity-70 shadow-2xl shadow-pink-500 blur max-md:left-6"
-      ></motion.div>
-      <motion.div
-        style={{ y: yIndigo }}
-        className="indigo-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500 opacity-70 shadow-2xl shadow-indigo-500 blur"
-      ></motion.div>
-      <motion.div
-        style={{ y: yGreen }}
-        className="green-bubble absolute right-1/3 top-28 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 opacity-70 shadow-2xl shadow-green-500 blur max-md:right-6 max-md:top-52 2xl:top-1/4"
-      ></motion.div>
-      <motion.div
-        style={{ y: yYellow }}
-        className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500 opacity-70 shadow-2xl shadow-yellow-500 blur"
-      ></motion.div>
-
+      <Bubbles></Bubbles>
       <div className="relative grid w-full grid-cols-1 pt-32 lg:grid-cols-7 xl:pt-40 2xl:grid-cols-2 2xl:pt-64">
         <div className="justify-between pt-16 max-sm:px-6 lg:col-span-3 2xl:col-span-1">
           <div className="pb-20 sm:py-20">
