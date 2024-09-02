4aa1463cf40e1330ee0c50435d82c1851b055506
Ragib Al Asad <ragibalasad@gmail.com>
2024-09-02 16:42:13 +0600

    Feat: Added AnimatedNumber reusable motion component

---
+++ components/motion/AnimatedNumber.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "../../src/lib/utils";
+import { motion, SpringOptions, useSpring, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
+type AnimatedNumber = {
+  value: number;
+  className?: string;
+  springOptions?: SpringOptions;
+};
+
+export function AnimatedNumber({
+  value,
+  className,
+  springOptions,
+}: AnimatedNumber) {
+  const spring = useSpring(value, springOptions);
+  const display = useTransform(spring, (current) =>
+    Math.round(current).toLocaleString(),
+  );
+
+  useEffect(() => {
+    spring.set(value);
+  }, [spring, value]);
+
+  return (
+    <motion.span className={cn("tabular-nums", className)}>
+      {display}
+    </motion.span>
+  );
+}
