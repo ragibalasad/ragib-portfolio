847a09fbad53e6cdc9c167828f0329646c462dbe
Ragib Al Asad <ragibalasad@gmail.com>
2024-09-08 23:11:41 +0600

    Added footer social link urls

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -10,8 +10,8 @@ import {
 const Footer = () => {
   return (
     <>
-      <section className="mt-24 bg-slate-900 dark:bg-slate-800 max-sm:px-6">
-        <div className="container mx-auto h-auto py-12 sm:w-4/5">
+      <section className="mt-24 bg-slate-900 dark:bg-slate-800">
+        <div className="container mx-auto h-auto py-12 max-sm:px-6 sm:w-4/5">
           <div className="flex gap-6 text-slate-300 max-lg:flex-col">
             <div className="my-auto">
               <h1 className="text-2xl font-medium text-cyan-400">
@@ -31,32 +31,33 @@ const Footer = () => {
                 <li>Contact</li>
               </ul>
               <div className="footer-social-links flex flex-wrap gap-6 text-lg lg:mx-auto">
-                <a href="">
+                <a href="https://x.com/ragibalasad" target="_blank">
                   <FaXTwitter />
                 </a>
-                <a href="">
+                <a href="https://facebook.com/ragibalasad" target="_blank">
                   <FaFacebook />
                 </a>
-                <a href="">
+                <a href="https://linkedin.com/in/ragibalasad" target="_blank">
                   <FaLinkedin />
                 </a>
-                <a href="">
+                <a href="https://github.com/ragibalasad" target="_blank">
                   <FaGithub />
                 </a>
-                <a href="">
-                  <FaInstagram />
+                <a href="https://discord.com/ragibalasad" target="_blank">
+                  <FaDiscord />
                 </a>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="bg-slate-950">
+        <div className="bg-slate-950 max-sm:px-6">
           <div className="container mx-auto h-auto py-3 sm:w-4/5">
             <div className="mx-auto w-fit text-slate-300">
               Copyright &copy; 2024. Made by{" "}
               <a
                 href="https://github.com/ragibalasad"
+                target="_blank"
                 className="font-medium text-cyan-400"
               >
                 Ragib Al Asad
@@ -70,11 +71,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// Copyright &copy; 2024. Made by{" "}
-// <a
-//   href="https://github.com/ragibalasad"
-//   className="font-medium text-cyan-400"
-// >
-//   Ragib Al Asad
-// </a>
