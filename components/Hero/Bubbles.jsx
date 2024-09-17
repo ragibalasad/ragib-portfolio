d5690681418f40e6a8826ea56387d42baf75fa90
Ragib Al Asad <ragibalasad@gmail.com>
2024-09-17 20:20:33 +0600

    Removed shadow and blur from bubbles

--- components/Hero/Bubbles.jsx
+++ components/Hero/Bubbles.jsx
@@ -24,19 +24,19 @@ const Bubbles = () => {
     <>
       <motion.div
         style={{ y: yPink }}
-        className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 shadow-2xl shadow-pink-500 blur max-md:left-6"
+        className="pink-bubble absolute left-10 top-6 -z-10 block h-48 w-48 rounded-full bg-gradient-to-br from-transparent to-pink-500 max-md:left-6"
       ></motion.div>
       <motion.div
         style={{ y: yIndigo }}
-        className="indigo-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500 shadow-2xl shadow-indigo-500 blur"
+        className="indigo-bubble absolute bottom-10 right-0 -z-10 block h-52 w-52 rounded-full bg-gradient-to-br from-transparent to-indigo-500"
       ></motion.div>
       <motion.div
         style={{ y: yGreen }}
-        className="green-bubble absolute right-1/3 top-28 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 shadow-2xl shadow-green-500 blur max-md:right-6 max-md:top-52 2xl:top-1/4"
+        className="green-bubble absolute right-1/3 top-28 -z-10 block h-40 w-40 rounded-full bg-gradient-to-br from-transparent to-green-500 max-md:right-6 max-md:top-52 2xl:top-1/4"
       ></motion.div>
       <motion.div
         style={{ y: yYellow }}
-        className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500 shadow-2xl shadow-yellow-500 blur"
+        className="yellow-bubble absolute -bottom-12 left-1/4 -z-10 block h-32 w-32 rounded-full bg-gradient-to-br from-transparent to-yellow-500"
       ></motion.div>
     </>
   );
