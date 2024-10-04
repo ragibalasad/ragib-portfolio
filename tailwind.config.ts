be078e7e87872fc4019d3c93ad801a1eccdfe32a
Ragib Al Asad <ragibalasad@gmail.com>
2024-10-05 02:51:11 +0600

    Migrate codebase to Typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const darkMode = "class";
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
